2755aa8b9bc64dbcb653043e8e2daf3217bc53d1
Daniel <zonatrialpeliculas@gmail.com>
2026-08-19 21:49:53 +0000

    Conectar APK a URL publica de Render

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/api";
+const API_URL = "https://app-recompensas-1.onrender.com";
 const USER_ID = "DANI123";
 const USD_PER_POINT = 0.0005;
 const MIN_WITHDRAWAL_USD = 5.00;
